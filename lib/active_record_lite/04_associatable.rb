2e7ca7836ea48ff9814f277f18b2696b2e13f3f9
App Academy Student <student@appacademy.io>
2014-04-18 16:25:15 -0700

    creating AssocOptions, BelongsToOptions, and HasManyOptions to set values for each of the values

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '03_searchable'
 require 'active_support/inflector'
+require 'debugger'
 
 # Phase IVa
 class AssocOptions
@@ -10,23 +11,29 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.to_s.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @name = name.to_sym
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.capitalize
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @name = name.to_sym
+    @foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s[0..-2].capitalize
   end
 end
 
