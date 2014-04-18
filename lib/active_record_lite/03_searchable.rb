8fc080f164941626dba21798ca610c8e827dd53a
App Academy Student <student@appacademy.io>
2014-04-18 15:31:13 -0700

    creating ::where method in Searchable module

--- lib/active_record_lite/03_searchable.rb
+++ lib/active_record_lite/03_searchable.rb
@@ -3,10 +3,27 @@ require_relative '02_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    attrs = params.keys.map do |attr|
+      "#{attr} = ?"
+    end.join(' AND ')
+
+    DBConnection.execute(<<-SQL, params.values)
+    SELECT
+    *
+    FROM
+    #{self.table_name}
+    WHERE
+    #{attrs}
+    SQL
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
+
+  def self.where(params)
+    results = super(params)
+    parse_all(results)
+  end
 end
